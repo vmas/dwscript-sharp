a34217b50f95b5663b79511736df6c4c273f03ef
vmas <none@none>
2013-12-08 01:32:20 +0400

    Добавлен недостающий файл

---
+++ DWScriptSharpUnitTests/CustomContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DWScript;
+using DWScript.Interop;
+
+namespace UnitTests
+{
+	class CustomContext : DWSContext
+	{
+		public Func<string, string> SourceGetter;
+		
+		public CustomContext(IDWSContext context)
+			: base(context)
+		{
+
+		}
+
+		protected override string OnInclude(string scriptName)
+		{
+			return SourceGetter != null ? SourceGetter(scriptName) : null;
+		}
+
+		
+
+	}
+}
